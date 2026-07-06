aa202d98d7efd85e698651b6225a344693144daa
nikitazhitin <zhna.ru@gmail.com>
2026-07-06 17:03:28 +0300

    refactor: rewrite rulers and guidelines on ReactiveNode engine

--- src/canvas/overlays/selection/SelectionElementBox.ts
+++ src/canvas/overlays/selection/SelectionElementBox.ts
@@ -13,8 +13,19 @@ export class SelectionElementBox extends BaseSelectionBox {
 
     const w = Math.hypot(corners[1].x - corners[0].x, corners[1].y - corners[0].y);
     const h = Math.hypot(corners[3].x - corners[0].x, corners[3].y - corners[0].y);
-    const angle = (Math.atan2(corners[1].y - corners[0].y, corners[1].x - corners[0].x) * 180) / Math.PI;
+    const angleRad = Math.atan2(
+      corners[1].y - corners[0].y,
+      corners[1].x - corners[0].x,
+    );
+    const angle = (angleRad * 180) / Math.PI;
+    const cos = Math.cos(angleRad);
+    const sin = Math.sin(angleRad);
+    const hw = w / 2;
+    const hh = h / 2;
 
-    this.setData(corners[0].x, corners[0].y, w, h, angle);
+    const x = corners[0].x - hw + hw * cos - hh * sin;
+    const y = corners[0].y - hh + hw * sin + hh * cos;
+
+    this.setData(x, y, w, h, angle);
   }
 }
